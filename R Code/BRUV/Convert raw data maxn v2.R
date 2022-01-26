cb913edace6d3fd4213337e71baadfab5da327a6
RossDwyer <drrossdwyer@gmail.com>
2022-01-26 16:32:53 +1000

    Update Max Ns to include BRUV metadata

--- R Code/BRUV/Convert raw data maxn v2.R
+++ R Code/BRUV/Convert raw data maxn v2.R
@@ -14,23 +14,29 @@ datafolder <- "/Users/uqrdwye2/Dropbox/shark_mpa_model_v400/SA/DEW Marine Parks 
 # load the BRUVS data
 maxndata <- read.csv(paste0(datafolder,"BRUVS data/SA.Synthesis.MaxN.csv"))
 
-unique(maxndata$genus.species)
+# load the BRUVS metadata
+maxnmeta <- read.csv(paste0(datafolder,"BRUVS data/SA.Synthesis.Metadata.csv"))
+
+# Combine the data and metadata files
+maxndata1 <- maxnmeta %>%
+  dplyr::select(sample,latitude,longitude,location,status,site,depth) %>%
+  right_join(maxndata)
+
 # Function to generate species specific bruv data with correct 'absences
 fextract_sp <- function(data,singlesp){
   
-  #data = maxndata
-  #singlesp = "Myliobatis tenuicaudatus"
-  
   maxndata_spec <- data %>% filter(genus.species ==singlesp)  # Extract data for single species
-  all_drops <- unique(as.character(data$sample))   # What are the names of the BRUV drops?
+  all_drops <- unique(as.character(data$sample))   # What are the names of the BRUV drops from all drops?
   
-  # Which drops were in a site with one of our named species, but didnt see any of this species on a survey? 
+  # Which drops were in a site with one of our named species, but didn't see any of this species on a survey? 
   # I.e. generates the 'absences'
+  
   maxndata_nospec <- data %>% filter(genus.species != singlesp)  # Extract data for single species
   nosspec_setids <- sort(unique(as.character(maxndata_nospec$sample))) # what are the set ids where species other than our fish speces were spotted  
   nosinglesp_setids <- nosspec_setids[nosspec_setids %!in% maxndata_spec$sample] # Extract set_ids not present in the single species data (i.e. absences)
   maxndata_spec_nospecdups <- data %>% filter(sample %in% nosinglesp_setids) # filter absences dataset for these set codes
   maxndata_spec_nospec<- maxndata_spec_nospecdups[!duplicated(maxndata_spec_nospecdups$sample),] # remove the duplicated set ids where more than one species was spotted
+  
   maxndata_spec_nospec <- maxndata_spec_nospec %>% mutate(family = "", # Add the missing columns and the Zeros for these BRUV surveys
                                                           genus = "",
                                                           species = "",
@@ -39,9 +45,6 @@ fextract_sp <- function(data,singlesp){
   
   #Assign the correct zeros to the shark bruv data dataframe (bind our shark species, other animals and no animals dataframes)
   maxndata_spec <- rbind(maxndata_spec,maxndata_spec_nospec) %>% arrange(sample) # Arrange by BRUV deployment id
-  
-  #are there any duplicates?
-  
   
   return(maxndata_spec)
 }
@@ -62,26 +65,39 @@ genus.sp <- c("Myliobatis tenuicaudatus", #Southern eagle ray
 
 # Run function to generate species specific bruv data with correct 'absences'
 # Also patch on common names to align with the Dispersal kernels dataset
-maxndata_spec1 <- fextract_sp(maxndata,genus.sp[1]) # Southern eagle ray
+maxndata_spec1 <- fextract_sp(maxndata1,genus.sp[1]) # Southern eagle ray
 maxndata_spec1 <- maxndata_spec1 %>% mutate(species_common_name="Southern eagle ray")
-maxndata_spec2 <- fextract_sp(maxndata,genus.sp[2]) # Harlequin Fish
+maxndata_spec2 <- fextract_sp(maxndata1,genus.sp[2]) # Harlequin Fish
 maxndata_spec2 <- maxndata_spec2 %>% mutate(species_common_name="Harlequin")
-maxndata_spec3 <- fextract_sp(maxndata,genus.sp[3]) # Yellowtail Kingfish
+maxndata_spec3 <- fextract_sp(maxndata1,genus.sp[3]) # Yellowtail Kingfish
 maxndata_spec3 <- maxndata_spec3 %>% mutate(species_common_name="Yellowtail Kingfish")
-maxndata_spec4 <- fextract_sp(maxndata,genus.sp[4]) # Snapper
+maxndata_spec4 <- fextract_sp(maxndata1,genus.sp[4]) # Snapper
 maxndata_spec4 <- maxndata_spec4 %>% mutate(species_common_name="Snapper")
-maxndata_spec5 <- fextract_sp(maxndata,genus.sp[5]) # Western Blue Groper 
+maxndata_spec5 <- fextract_sp(maxndata1,genus.sp[5]) # Western Blue Groper 
 maxndata_spec5 <- maxndata_spec5 %>% mutate(species_common_name="Western Blue Groper")
-maxndata_spec6 <- fextract_sp(maxndata,genus.sp[6]) # Bluethroat Wrasse 
+maxndata_spec6 <- fextract_sp(maxndata1,genus.sp[6]) # Bluethroat Wrasse 
 maxndata_spec6 <- maxndata_spec6 %>% mutate(species_common_name="Bluethroat Wrasse")
-maxndata_spec7 <- fextract_sp(maxndata,genus.sp[7]) # Bronze Whaler
+maxndata_spec7 <- fextract_sp(maxndata1,genus.sp[7]) # Bronze Whaler
 maxndata_spec7 <- maxndata_spec7 %>% mutate(species_common_name="Bronze whaler")
-maxndata_spec8 <- fextract_sp(maxndata,genus.sp[8]) # Dusky Whaler 
+maxndata_spec8 <- fextract_sp(maxndata1,genus.sp[8]) # Dusky Whaler 
 maxndata_spec8 <- maxndata_spec8 %>% mutate(species_common_name="Dusky whaler")
-maxndata_spec9 <- fextract_sp(maxndata,genus.sp[9]) # Silver Trevally
+maxndata_spec9 <- fextract_sp(maxndata1,genus.sp[9]) # Silver Trevally
 maxndata_spec9 <- maxndata_spec9 %>% mutate(species_common_name="silver trevally")
-maxndata_spec10 <- fextract_sp(maxndata,genus.sp[10]) # White Shark
+maxndata_spec10 <- fextract_sp(maxndata1,genus.sp[10]) # White Shark
 maxndata_spec10 <- maxndata_spec10 %>% mutate(species_common_name="White shark")
+
+# Check raw data
+check0 <- maxndata1 %>% filter(genus.species=="Chrysophrys auratus") #records with at least one of species seen 
+dim(check0) # number of times species 'present'
+sum(check0$maxn) # Total no animals seen
+
+# Check against my data
+check1 <- maxndata_spec4 %>% filter(maxn>0) #records with at least one of species seen 
+head(check1,1)
+dim(check1) # number of times species 'present'
+sum(check1$maxn) # Total no animals seen
+
+## Looks good!
 
 # Combine into single dataset
 maxndata_spec_all <- list(maxndata_spec1,
